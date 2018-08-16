880e2061518795eab505ef1d394ec49006002488
kimhs2582 <42165242+kimhs2582@users.noreply.github.com>
2018-08-16 15:00:24 +0900

    초기작업_03_수정필

--- typingGame/WordLv1.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-string lv1[Dif] = 
-{
-	"단어","낱말","감자"
-}
--- typingGame/main.cpp
+++ typingGame/main.cpp
@@ -8,13 +8,17 @@ int main()
 {
 
 	// 미리 레벨에 맞는 단어 저장
-	char Lv1[512] = {"나라,국가,날림,엄마,그림,파리"};
+	char Lv[5][512] = { {"나라,국가,날림,엄마,그림,파리,모기,사과"},{ "나달,날두,아크,담배,아구,대리,감귤" } ,{"가가,나나,다다,라라,마마,바바,사사"},
+	{"갸갸,거거,겨겨,고고,교교,구구,규규"},{"나다,다라,마바,바사,사아,아자"} };
+	
+
 	char answer[512] = {};
 	char i;
 	int lv, Exp, life, comp,difficulty,bonus,bExp,blife;
 	char* token;												//추가함
 	// 초기 설정
 	lv = 1;
+	
 	Exp = 0;
 	life = 3;
 	comp = 0;
@@ -22,7 +26,6 @@ int main()
 	bExp = 0;
 	blife = 0;
 
-	token = strtok(Lv1,",");									//추가함
 	// 뭔가 없어보이는 초기 화면
 	cout << "typingGame" << endl;
 	cout << endl;
@@ -41,7 +44,8 @@ int main()
 	//반복
 	while (1)
 	{
-		
+		token = strtok(Lv[lv - 1], ",");									//수정해야함
+
 		// 승리 여부
 		if (lv == 6)
 		{
@@ -56,6 +60,7 @@ int main()
 			bExp = 1;
 		else if (bonus == 7)
 			blife = 1;
+
 
 		 
 		//일반문제 단어 출력 및 입력(레벨 확인)
@@ -113,8 +118,8 @@ int main()
 		//정답 여부 판별후 정산
 		if (strcmp(token, answer) == 0)
 		{
-			system("cls");
-			cout << "성공" << endl;
+			system("cls");// 테스트용
+			cout << "성공" << endl;// 테스트용
 			Exp++;
 			if (bExp == 1)
 			{
@@ -129,27 +134,29 @@ int main()
 		}
 		else
 		{
-			system("cls");
-			cout << "실패" << endl;
+			system("cls");// 테스트용
+			cout << "실패" << endl;// 테스트용
 			bExp = 0;
 			blife = 0;
 			life--;
 		}
+		//결과 출력
 		cout << "lv" << lv << endl;
 		cout<<"exp" << Exp << endl;
 		cout<<"life"<< life << endl;
+		cout << endl << endl << endl << endl;
 
 
 		//레벨업 가능 판별 후 가능이면 레벨업
-		if (lv == 1 && Exp == 3)
+		if (lv == 1 && Exp >= 3)
 			lv++;
-		else if (lv == 2 && Exp == 9)
+		else if (lv == 2 && Exp >= 9)
 			lv++;
-		else if (lv == 3 && Exp == 18)
+		else if (lv == 3 && Exp >= 18)
 			lv++;
-		else if (lv == 4 && Exp == 30)
+		else if (lv == 4 && Exp >= 30)
 			lv++;
-		else if (lv == 5 && Exp == 45)
+		else if (lv == 5 && Exp >= 45)
 			lv++;
 
 
